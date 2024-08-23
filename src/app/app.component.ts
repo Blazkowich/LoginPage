de609eedf7abd7d75c1144e5ac7d06e848aecdf9
Blazkowich <oiluridze@gmail.com>
2024-08-23 21:24:51 +0400

    Base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EyeComponent } from "./eye/eye.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EyeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { EyeComponent } from './eye/eye.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: EyeComponent },
+];
